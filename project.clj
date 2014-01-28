760badccdad00df9e3b7fa7b20867f0933fe0dd6
Juho Teperi <juho.teperi@iki.fi>
2014-01-28 13:45:07 +0200

    Fix

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Swagger-UI for Ring apps"
   :url "https://github.com/metosin/ring-swagger-ui"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
