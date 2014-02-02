a74120d313716c355c61e68bd4ad3b3acfe5f1b9
Tommi Reiman <tommi.reiman@metosin.fi>
2014-02-02 21:13:03 +0200

    build to target-directory

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :resource {:resource-paths ["ext/swagger-ui/dist"]
-             :target-path "resources/swagger-ui"
-             :excludes [#"ext/swagger-ui/dist/index\.html"]}
+  :resource {:resource-paths ["ext/swagger-ui/dist" "resources/swagger-ui"]
+             :target-path "target/resources/swagger-ui"
+             :excludes [#"ext/swagger-ui/dist/index\.html" #".*\.DS_Store"]}
+  :resource-paths ["target/resources"]
   :hooks [leiningen.resource]
   :profiles {:dev {:plugins [[lein-clojars "0.9.1"]
                              [lein-resource "0.3.3"]]}})
