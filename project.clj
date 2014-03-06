63a0543fb203a4ff3e0dcf1f419289de82734062
Tommi Reiman <tommi@iki.fi>
2014-03-06 22:03:00 +0200

    2.0.12-1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger-ui "2.0.10-1"
+(defproject metosin/ring-swagger-ui "2.0.12-1"
   :description "Swagger UI for Ring apps"
   :url "https://github.com/metosin/ring-swagger-ui"
   :license {:name "Eclipse Public License"
