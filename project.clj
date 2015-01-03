0e4bf80b1f3304de6e3889f08a4960b6fb721171
Juho Teperi <juho.teperi@iki.fi>
2015-01-03 20:34:05 +0200

    Release 2.0.24.
    - Index.html should be as near as possible to upstream version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger-ui "2.0.17"
+(defproject metosin/ring-swagger-ui "2.0.24"
   :description "Swagger UI for Ring apps"
   :url "https://github.com/metosin/ring-swagger-ui"
   :license {:name "Eclipse Public License"
