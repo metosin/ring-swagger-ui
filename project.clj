7ae3296ac9dfc03e4e91d9af687f9f0b8dcc9b6c
Juho Teperi <juho.teperi@iki.fi>
2017-03-21 18:26:37 +0200

    Test new Swagger UI 3.0.2

--- project.clj
+++ project.clj
@@ -1,14 +1,19 @@
-(defproject metosin/ring-swagger-ui "2.2.10"
+(defproject metosin/ring-swagger-ui "3.0.2-SNAPSHOT"
   :description "Swagger UI for Ring apps"
   :url "https://github.com/metosin/ring-swagger-ui"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :resource {:resource-paths ["ext/swagger-ui/dist" "resources/swagger-ui"]
+  :resource {:resource-paths ["ext/swagger-ui/dist" "resources/swagger-ui"
+                              "ext/swagger-ui/public" "resources/swagger-ui"]
              :target-path "target/resources/swagger-ui"
              :skip-stencil [ #".*" ]
-             :excludes [#"ext/swagger-ui/dist/index\.html" #".*\.DS_Store"]}
+             :includes [#".*/favicon.*\.png"
+                        #".*/swagger-ui\.css.*"
+                        #".*/swagger-ui-.*\.js.*"
+                        #".*/.*\.html"]
+             :excludes [#"ext/.*/index\.html"]}
   :resource-paths ["target/resources"]
   :hooks [leiningen.resource]
   :profiles {:dev {:plugins [[lein-resource "14.10.1"]]}})
