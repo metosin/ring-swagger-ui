8a8edfe0269464a4c7bbf88b5ff852426c37bf1d
ejschoen <ejschoen@i2kconnect.com>
2018-06-30 11:43:30 -0500

    Sync version with swagger-ui

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger-ui "3.0.18-SNAPSHOT"
+(defproject metosin/ring-swagger-ui "3.17.1"
   :description "Swagger UI for Ring apps"
   :url "https://github.com/metosin/ring-swagger-ui"
   :license {:name "Eclipse Public License"
