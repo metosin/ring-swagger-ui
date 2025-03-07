bdc9d762ea01f73d0996dd620b8fa6a7b567e2a4
Juho Teperi <juho.teperi@iki.fi>
2025-03-07 10:52:52 +0200

    Version v5.20.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/ring-swagger-ui "5.18.2"
+(defproject metosin/ring-swagger-ui "5.20.0"
   :description "Swagger UI for Ring apps"
   :url "https://github.com/metosin/ring-swagger-ui"
   :license {:name "Eclipse Public License"
